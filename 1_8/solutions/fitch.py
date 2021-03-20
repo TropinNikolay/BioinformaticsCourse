9aed438641cafe43075e1d26adab6a93fd29c816
Nikolay Tropin <tropinnikolay@gmail.com>
2021-03-20 20:49:43 +0300

    Added fitch algorithm implementation

--- 1_8/solutions/fitch.py
+++ 1_8/solutions/fitch.py
@@ -1,16 +1,55 @@
 from Bio import Phylo
 
 
+def transform_set_to_string(node):
+    """
+    Useful function for debugging
+    """
+    node.name = str(node.name)
+    if node.is_terminal():
+        return
+
+    up_node, down_node = node.clades
+    transform_set_to_string(up_node)
+    if down_node:
+        transform_set_to_string(down_node)
+
+
 def forward(node):
-    pass
+    if node.is_terminal():
+        node.name = set(node.name)
+        return
+
+    up_node, down_node = node.clades
+    forward(up_node)
+    node.name = up_node.name
+    if down_node:
+        forward(down_node)
+        intersect = node.name.intersection(down_node.name)
+        union = node.name.union(down_node.name)
+        node.name = intersect if intersect else union
 
 
-def backward(node):
-    pass
+def backward(root):
+    root.name = root.name.pop()
+    if root.is_terminal():
+        return
+
+    up_node, down_node = root.clades
+    if len(up_node.name) > 1:
+        up_node.name = up_node.name.intersection(root.name)
+
+    if down_node:
+        if len(down_node.name) > 1:
+            down_node.name = down_node.name.intersection(root.name)
+
+    backward(up_node)
+    backward(down_node)
 
 
 def fitch(tree):
     forward(tree.clade)
+    # transform_set_to_string(tree.clade)
     backward(tree.clade)
     return tree
 
@@ -18,4 +57,4 @@ def fitch(tree):
 if __name__ == '__main__':
     tree = Phylo.read("newick_tree_example.txt", format="newick")
     tree = fitch(tree)
-    Phylo.draw_ascii(tree)
+    Phylo.draw(tree)
